06cd196851351eedcb9c298f362e65253e1fb93f
Isabella <isabella.soares.28@gmail.com>
2020-06-04 22:30:28 -0300

    add first routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,30 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = ['Diego', 'Cleiton', 'Robson'];
+
 app.get('/users', (request, response) => {
-  console.log('listagem de usuários');
+  const search = String(request.query.search);
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
 
-  // response.send('Hello world!');
-
-  response.json(['Diego', 'Cleiton', 'Robson']);
+  response.json(filteredUsers);
 });
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const user = users[id];
+  response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+  const user = { name: data.name, email: data.email };
+
+  return response.json(user);
+});
+
 app.listen(3333);
