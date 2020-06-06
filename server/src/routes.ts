864257777f5524c70c233ba1df80ad014ab70ae2
Isabella <isabella.soares.28@gmail.com>
2020-06-06 18:54:57 -0300

    add items routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  response.json({ message: 'Hello world' });
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map((item) => ({
+    title: item.title,
+    image_url: `http://localhost:3333/uploads/${item.image}`,
+  }));
+
+  return response.json(serializedItems);
 });
 
 export default routes;
