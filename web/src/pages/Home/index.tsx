564f621d894014ebe76641f8a08534a79a44507d
Isabella <isabella.soares.28@gmail.com>
2020-06-07 15:34:45 -0300

    add routes

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo.svg';
 import './styles.css';
@@ -17,12 +17,12 @@ const Home = () => {
           <p>
             Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
           </p>
-          {/* <Link to="/create-point"> */}
-          <span>
-            <FiLogIn />
-          </span>
-          <strong>Cadastre um ponto de coleta</strong>
-          {/* </Link> */}
+          <Link to="/create-point">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </Link>
         </main>
       </div>
     </div>
